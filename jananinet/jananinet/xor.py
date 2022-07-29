eea024f61b17453190ad0801a3fc0f0e34582ee8
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-07-29 12:29:11 -0400

    Add files via upload

---
+++ jananinet/jananinet/xor.py
@@ -0,0 +1,35 @@
+"""
+The canonical example of a function that cant be learned
+with a simple linear model is XOR
+"""
+
+import numpy as np
+from jananinet.train import train
+from jananinet.nn import NeuralNet
+from jananinet.layers import Linear, Tanh
+
+inputs = np.array([
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1]
+])
+
+targets = np.array([
+    [1, 0],
+    [0, 1],
+    [0, 1],
+    [1, 0]
+])
+
+net = NeuralNet([
+    Linear(input_size=2, output_size=2),
+    Tanh(),
+    Linear(input_size=2, output_size=2)
+])
+
+train(net, inputs, targets)
+
+for x, y in zip(inputs, targets):
+    predicted = net.forward(x)
+    print(x, predicted, y)
